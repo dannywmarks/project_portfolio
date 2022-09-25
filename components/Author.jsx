89fd1275986d71b18c5be8326519de9d0d84819c
Danny Marks <dannywmarks@gmail.com>
2022-09-25 10:48:46 -0700

    author comp and blog detail comp complete

--- components/Author.jsx
+++ components/Author.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import Image from 'next/image'
 
-const Author = () => {
+const Author = ({author}) => {
   return (
-    <div>Author</div>
+    <div className="text-center mt-20 mb-8 p-12 relative rounded-lg bg-gray-900 bg-opacity-80">
+      <div className="left-0 right-0 -top-14 absolute">
+      <Image className="align-middle rounded-full" src={author.photo.url} alt={author.name} height="100px" width="100px" unoptimized/>
+    
+      </div>
+      <h3 className="text-white my-4 text-xl font-bold">
+        {author.name}
+      </h3>
+      <p className="text-white text-lg">{author.bio}</p>
+    </div>
   )
 }
 
